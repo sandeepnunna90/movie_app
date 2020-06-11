4e054a909f754016e02aeb6becd74b434cba9b92
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-06-10 21:43:18 -0400

    Allow users to only add one review per user

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,6 +15,7 @@ class MoviesController < ApplicationController
   # GET /movies/1.json
   def show
     @reviews = Review.where(movie_id: @movie.id).order('created_at DESC')
+    @user_review = Review.where(movie: @movie, user: current_user)
   end
 
   # GET /movies/new
