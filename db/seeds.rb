eb0ec5ce938f9b2bb93fb8bd87731745e4c75277
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-06-10 22:25:38 -0400

    Upgrade to use postgresql database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,7 +8,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: "sandeep@example.com",
-             password: "password",
-             password_confirmation: "password",
+User.create!(email: 'admin@gmail.com',
+             password: 'password',
+             password_confirmation: 'password',
              admin: true)
